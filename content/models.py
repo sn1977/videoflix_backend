2321852955029dc61f84e060f59e5f2e8020aece
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-06 12:11:23 +0200

    feat: Add Video model and register it in admin.py

--- content/models.py
+++ content/models.py
@@ -1,3 +1,12 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+  created_at = models.DateField(default=date.today)
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=500)
+  video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+
+def __str__(self):
+    return self.title
--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +68,9 @@ TEMPLATES = [
         },
     },
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'videoflix_backend.wsgi.application'
 
