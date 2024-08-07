6c8a4694baf8f75cad35ab607eeff5a0ad426890
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-07 21:00:57 +0200

    feat: Add CustomUserAdmin class to users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-
+from .forms import CustomUserCreationForm
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        
+        (
+            "Custom Fields",
+            {
+                "fields": (
+                    "custom",
+                    "phone",
+                    "street",
+                    "postal_code",
+                    "city",
+                )
+            },
+        ),
+        *UserAdmin.fieldsets,
+    )
