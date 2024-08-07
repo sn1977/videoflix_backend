05308835b6f77b68a423696445bc5f427be4c75e
Sascha Nemeth <sascha.nemeth@me.com>
2024-08-07 19:37:51 +0200

    feat: Register Video model in admin.py and add import/export functionality

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Video
 
-# Register your models here.
-admin.site.register(Video)
+class VideoResource(resources.ModelResource):
+
+    class Meta:
+        model = Video
+
+@admin.register(Video)
+class VideoAdmin(ImportExportModelAdmin):
+    pass
